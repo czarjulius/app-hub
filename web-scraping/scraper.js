e2c3ab45bb2ea946ffbf750b24c8c19256e6c8e7
czarjulius <juliusczar.jc@gmail.com>
2023-09-12 11:32:44 +0100

    automated web scraping with puppeteer

--- web-scraping/scraper.js
+++ web-scraping/scraper.js
@@ -1,29 +1,46 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const fileName = 'orla_data.json';
+
+if (fs.existsSync(fileName)) {
+  fs.unlinkSync(fileName);
+}
 
 async function runScraper() {
   const browser = await puppeteer.launch({ headless: 'old' });
-  const page = await browser.newPage();
+  try {
+    const page = await browser.newPage();
 
-  await page.goto('https://orla.africa/');
+    await page.goto('https://orla.africa/');
+    console.log('Web scraping started...🎬');
 
-  // await page.screenshot({ path: 'orla.png', fullPage: true });
-  // await page.pdf({ path: 'orla.pdf', format: 'A4' });
-  // const html = await page.content();
+    await new Promise((r) => setTimeout(r, 20000));
 
-  // const title = await page.evaluate(() => document.title);
-  const gadgets = await page.$$eval(
-    '._overflowCard_1cymp_30 ._productCard_1cymp_12',
-    (gadgets) =>
-      gadgets.map((item) => ({
-        title: item.querySelector('h4').innerText,
-        imageUrl: item.querySelector('img').src,
-        price: item.querySelector('strong').innerText,
-      }))
-  );
+    // await page.screenshot({ path: 'orla.png', fullPage: true });
+    // await page.pdf({ path: 'orla.pdf', format: 'A4' });
+    // const html = await page.content();
 
-  console.log(gadgets);
+    const title = await page.evaluate(() => document.title);
+    const gadgets = await page.$$eval(
+      '._overflowCard_1cymp_30 ._productCard_1cymp_12',
+      (gadgets) =>
+        gadgets.map((item) => ({
+          title: item.querySelector('h4').innerText,
+          imageUrl: item.querySelector('img').src,
+          price: item.querySelector('strong').innerText,
+        }))
+    );
 
-  await browser.close();
+    const orla_data_json = { title: title, products: gadgets };
+
+    fs.writeFileSync(fileName, JSON.stringify(orla_data_json));
+
+    console.log('Web scraping completed successfully ✅✅✅');
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await browser.close();
+  }
 }
-
 module.exports = runScraper;
--- web-scraping/server.js
+++ web-scraping/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+
 const runScraper = require('./scraper');
 
 const app = express();
 const PORT = 8000;
 
 app.listen(PORT, async function onListen() {
-  // testSequelize(); // test the sequelize connection.'
   runScraper();
   console.log(`Server is up and running on port ${PORT}`);
 });
