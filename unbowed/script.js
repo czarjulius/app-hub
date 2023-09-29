649ecc5660eeb63eeb9cce02841bdc9cdc174ef3
czarjulius <juliusczar.jc@gmail.com>
2023-09-29 10:34:39 +0100

    finished unbowed

--- unbowed/script.js
+++ unbowed/script.js
@@ -24,7 +24,7 @@ const playVoice = async () => {
   if ('speechSynthesis' in window) {
     var utterance = new SpeechSynthesisUtterance();
     utterance.text = text;
-    utterance.pitch = 1;
+    utterance.pitch = 1.3;
     utterance.volume = 3;
     utterance.rate = 0.65;
     window.speechSynthesis.speak(utterance);
