850167102bacb5fa170e546455fec0d47cb316c9
czarjulius <juliusczar.jc@gmail.com>
2023-11-28 16:34:32 +0100

    port

--- web-scraping/server.js
+++ web-scraping/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const runScraper = require('./scraper');
 
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 
 app.listen(PORT, async function onListen() {
   runScraper();
